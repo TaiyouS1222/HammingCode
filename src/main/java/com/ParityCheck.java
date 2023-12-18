d3a663f9c657885e28b0c2d413bc26320f8264bd
user <ch9412en@gmail.com>
2023-12-18 10:48:56 +0800

    commit

--- src/main/java/com/ParityCheck.java
+++ src/main/java/com/ParityCheck.java
@@ -6,13 +6,15 @@ import static main.java.com.BoolToInt.boolToInt;
 
 public class ParityCheck {
     public static void main(String[] args){
-        Random random = new Random();
+        //生成隨機數據
         RandomDataCreater rdc = new RandomDataCreater();
         BoolToInt boolToInt = new BoolToInt();
         Boolean dataA[] = new Boolean[16];
         rdc.createData(dataA);
+        //原數據內容
         System.out.print("原數據:");
         Arrays.stream(dataA).forEach(x -> System.out.print(boolToInt(x)));
+        //計算數據且更改效驗碼
         int count = 0;
         for (int i = 1;i<dataA.length;i++){
             if(dataA[i] == true){
@@ -24,6 +26,7 @@ public class ParityCheck {
         }else {
             dataA[0] = true;
         }
+        //輸出新數據
         System.out.print("\n新數據:");
         Arrays.stream(dataA).forEach(x -> System.out.print(boolToInt(x)));
     }
